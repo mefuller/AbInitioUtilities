e6555dbffa88544e4468048d2b9a9f43cb956dde
Mark Fuller <mark.e.fuller@gmx.de>
2020-04-22 10:40:54 +0200

    License updates to GPL and headers on files prior to wider sharing with PCFC members

--- TAMkin/get_energy.py
+++ TAMkin/get_energy.py
@@ -1,3 +1,37 @@
+# 
+#    get_energy.py: a python function to read and return the optimized energy
+#    Combines zero point energy from geometry/frequency calculation with single-point energy result
+#    Usage: energy=get_energy(mol_name,mol_loc)
+#    mol_loc is path to directory where files are located
+#    mol_name is species identifier; files names are species followed by method and basis
+#    Currently written assuming methods (b2plypd3-ccpvtz//f12-tz) with Gaussian and Orca 
+#    Copyright (C) 2020  Mark E. Fuller
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    Mark E. Fuller: mark.e.fuller@gmx.de
+
+
+#setup terminal output later:
+#    get_energy.py  Copyright (C) 2020  Mark E. Fuller
+#    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+#    This is free software, and you are welcome to redistribute it
+#    under certain conditions; type `show c' for details.
+
+
+################################################################################
+
 """
 M. E. Fuller, 5 April 2020
 
