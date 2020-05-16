4b76737335c425bc6bc40dc7bec295e7b3a2e5b6
Mark Fuller <mark.e.fuller@gmx.de>
2020-05-16 22:29:44 +0200

    Tamkin ORCA energy and unit test

--- TAMkin/test_orca_singlepointenergy.py
+++ TAMkin/test_orca_singlepointenergy.py
@@ -1,13 +1,11 @@
 import unittest
+from get_energy_orca import get_energy_orca
 
 
 class TestOrcaSPE(unittest.TestCase):
 
     def test_orca_spe(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_spe_zpe(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+        self.assertEqual(get_energy_orca("hno_spe.out"), -130.348214495060, "Should be -130.348214495060")
 
 if __name__ == '__main__':
     unittest.main()
